09445aac1adf9472d0ec0b203e204819f68f08a0
Aline Buchino <aline.buchino@hotmail.com>
2022-07-04 22:35:08 -0300

    transação para o JpaRepository com Spring Data

--- io/github/alinebuchino/VendasApplication.java
+++ io/github/alinebuchino/VendasApplication.java
@@ -18,30 +18,33 @@ public class VendasApplication {
     public CommandLineRunner init(@Autowired Clientes clientes) {
         return args -> {
             System.out.println("Salvando clientes");
-            clientes.salvar(new Cliente("Aline"));
-            clientes.salvar(new Cliente("Teste"));
+            clientes.save(new Cliente("Aline"));
+            clientes.save(new Cliente("Teste"));
 
-            List<Cliente> todosClientes = clientes.obterTodos();
+            List<Cliente> result = clientes.encontrarPorNome("Aline");
+            result.forEach(System.out::println);
+
+            List<Cliente> todosClientes = clientes.findAll();
             todosClientes.forEach(System.out::println);
+//
+//            System.out.println("Atualizando clientes");
+//            todosClientes.forEach(c -> {
+//                c.setNome(c.getNome() + " atualizado.");
+//                clientes.save(c);
+//            });
+//
+//            todosClientes = clientes.findAll();
+//            todosClientes.forEach(System.out::println);
+//
+//            System.out.println("Buscando clientes");
+//            clientes.findByNomeLike("Aline").forEach(System.out::println);
 
-            System.out.println("Atualizando clientes");
-            todosClientes.forEach(c -> {
-                c.setNome(c.getNome() + " atualizado.");
-                clientes.atualizar(c);
-            });
+//            System.out.println("deletando clientes");
+//            clientes.findAll().forEach(c -> {
+//                clientes.delete(c);
+//            });
 
-            todosClientes = clientes.obterTodos();
-            todosClientes.forEach(System.out::println);
-
-            System.out.println("Buscando clientes");
-            clientes.buscarPorNome("Aline").forEach(System.out::println);
-
-            System.out.println("deletando clientes");
-            clientes.obterTodos().forEach(c -> {
-                clientes.deletar(c);
-            });
-
-            todosClientes = clientes.obterTodos();
+            todosClientes = clientes.findAll();
             if(todosClientes.isEmpty()){
                 System.out.println("Nenhum cliente encontrado.");
             }else{
--- io/github/alinebuchino/domain/repository/Clientes.java
+++ io/github/alinebuchino/domain/repository/Clientes.java
@@ -1,60 +1,27 @@
 package io.github.alinebuchino.domain.repository;
 
 import io.github.alinebuchino.domain.entity.Cliente;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-@Repository // uma classe que acessa a base de dados
-public class Clientes {
+public interface Clientes extends JpaRepository<Cliente, Integer> {
 
-    @Autowired
-    private EntityManager entityManager;
+    //List<Cliente> findByNomeLike(String nome);
+    //List<Cliente> findByNomeOrId(String nome, Integer id); // buscar por nome ou id
+    //List<Cliente> findByNomeOrIdOrderById(String nome, Integer id); // buscar por nome ou id ordenando por Id
+    //boolean existsByNome(String nome); // verificar se o nome existe ... etc
 
-    @Transactional // diz que o método terá uma transição com BD
-    public Cliente salvar(Cliente cliente) {
-        entityManager.persist(cliente);
-        return cliente;
-    }
+    @Query(value = " select * from cliente c where c.nome like '%:nome%' ", nativeQuery = true)
+    List<Cliente> encontrarPorNome( @Param("nome") String nome );
 
-    @Transactional
-    public Cliente atualizar(Cliente cliente) {
-        entityManager.merge(cliente);
-        return cliente;
-    }
+    @Query(" delete from Cliente c where c.nome =:nome ")
+    @Modifying // indica que o método não é apenas uma busca, precisa de uma transação no BD
+    void deleteByNome(String nome);
 
-    @Transactional
-    public void deletar(Cliente cliente){ // deletar por nome
-        if (!entityManager.contains(cliente)) {
-            cliente = entityManager.merge(cliente);
-        }
-        entityManager.remove(cliente);
-    }
+    boolean existsByNome(String nome);
 
-    @Transactional
-    public void deletar(Integer id){ // deletar por id
-       Cliente cliente = entityManager.find(Cliente.class, id);
-        deletar(cliente);
-    }
-
-    @Transactional(readOnly = true) // transação é apenas leitura (não é obrigatório é mais rápido)
-    public List<Cliente> buscarPorNome(String nome){
-        String jpql = "select c from Cliente c where c.nome like :nome";
-        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
-        query.setParameter("nome", "%" + nome + "%");
-        return query.getResultList();
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> obterTodos(){
-        return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
-    }
 }
