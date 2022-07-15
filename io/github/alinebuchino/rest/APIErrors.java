59f10ebfa9f71518fb1c009eba2d372ead648d7e
Aline Buchino <aline.buchino@hotmail.com>
2022-07-14 22:40:58 -0300

    APIErrors

--- io/github/alinebuchino/rest/APIErrors.java
+++ io/github/alinebuchino/rest/APIErrors.java
@@ -1,2 +1,16 @@
-package io.github.alinebuchino.rest;public class APIErrors {
+package io.github.alinebuchino.rest;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class APIErrors {
+
+    @Getter
+    private List<String> errors;
+
+    public APIErrors(String mensagemErro) {
+        this.errors = Arrays.asList(mensagemErro);
+    }
 }
